bedded7b28e34a088c9540650a6265d112d2ccc2
Aman <amanjangra.tech@gmail.com>
2022-09-15 16:47:53 +0530

    testing

--- Jest testing/routes&Function.test.js
+++ Jest testing/routes&Function.test.js
@@ -12,26 +12,7 @@ jest.mock('../database/databaseOps');
 jest.setTimeout(100000)
 
 
-// const mockConnection = {
-//     connect: jest.fn().mockResolvedValue()
-// };
 
-// pool= jest.fn().mockResolvedValue(mockConnection);
-
-
-// test("Testing getStudentById request from database", async ()=>{
-//     let query= `select * from StudentDetails where StudentId = @studentId`
-//     let parameters = [['studentId',sql.NVarChar,'1']]
-//     let result = await querySp.executeQueryString(query,parameters)
-//     expect(result).toEqual(studentByIdOutput)
-// })
-
-
-// test("Testing getStudentById request from database", async ()=>{
-//     const id = 1
-//     let res = (await database.getStudentById(id))
-//     console.log(res)
-// })
 
 
 
@@ -53,14 +34,3 @@ test("testing for insert student details", async ()=>{
 })
 
 
-// test("testing for stored proc", async ()=>{
-//     const student_info=[
-//         ["StudentId", sql.NVarChar ,"1137"],
-//         ["StudentName",sql.NVarChar, "Saket Sharma"],
-//         ["GPA",sql.Float, 9],
-//         ["Branch",sql.NVarChar, "CS"],
-//         ["Section",sql.NVarChar, "C"]
-//     ]
-//     // querySp.executeStoredProcedure = jest.fn()
-//     expect(querySp.executeStoredProcedure(storedProcToInsert,student_info)).toBe()
-// })
